f95112461327a3cc4c672c4f6887c076a6c60ce0
JuniorCesar23 <cesarck36@gmail.com>
2021-04-30 16:11:34 -0300

    Alterando a classe de inseri para ler os dados do usuário

--- src/mysql/InserirDados.java
+++ src/mysql/InserirDados.java
@@ -3,11 +3,14 @@ package mysql;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 import conexao.FecharStatement;
 import conexao.IniciarConexao;
 
 public class InserirDados {
+
+    private static Scanner input = new Scanner(System.in);
 
     private static PreparedStatement state = null;
 
@@ -20,12 +23,20 @@ public class InserirDados {
             state = conexao.prepareStatement("insert into pessoas (email, senha)" 
                             + " values (?, ?) ");
 
-            state.setString(1, "junior@gmail.com");
-            state.setString(2, "123456");
+            System.out.println("=== REALIZAR CADASTRO ===");
+
+            System.out.println("Seu E-mail: ");
+                String email = input.next();
+
+            System.out.println("Crie sua senha: ");
+                String senha = input.next();
+
+            state.setString(1, email);
+            state.setString(2, senha);
 
             int linhasAfetadas = state.executeUpdate();
 
-            System.out.println("Pronto! Linhas afetadas: " + linhasAfetadas);
+            System.out.println("Cadastro realizado com sucesso! Linhas afetadas: " + linhasAfetadas);
 
         } 
         catch (SQLException e) {
