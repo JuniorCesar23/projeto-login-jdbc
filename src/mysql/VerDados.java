b9c54d1f70b5a6c36d3cd7dd7f8f0b3a05f516c6
JuniorCesar23 <cesarck36@gmail.com>
2021-04-30 15:40:54 -0300

    Criacao da classe responsavel por imprimir os dados do banco

---
+++ src/mysql/VerDados.java
@@ -0,0 +1,33 @@
+package mysql;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import conexao.IniciarConexao;
+
+import conexao.ExcessaoBanco;
+
+public class VerDados {
+    
+    private static Statement state = null;
+    private static ResultSet rs = null;
+
+    public static void verDados(Connection conexao){
+        try {
+            conexao = IniciarConexao.iniciarConexao();
+            state = conexao.createStatement();
+            rs = state.executeQuery("select * from pessoas");
+
+            System.out.println("===DADOS DA TABELA===");
+
+            while (rs.next()){
+                System.out.println("Email: " + rs.getString("email"));
+            }
+
+        } catch (Exception e) {
+            throw new ExcessaoBanco(e.getMessage());
+        }
+    }
+
+}
